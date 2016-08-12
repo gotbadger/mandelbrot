367d0bd11bbc8c410582fac97e77cab866e2cb24
Philip Hayton <phil@freeagent.com>
2016-08-12 20:48:14 +0100

    added command line args updated readme

--- README.md
+++ README.md
@@ -7,3 +7,34 @@ mandelbrot in go
 go get -u github.com/gotbadger/mandelbrot && mandelbrot
 ```
 
+### Options:
+
+you can see all options by running `mandelbrot -h`
+
+```
+Usage of mandelbrot:
+  -i int
+       	max number of iterations / colours (default 30)
+  -step float
+       	a pixel is drawn for each step between coordinates (default 0.003)
+  -x0 float
+       	from X (default -2)
+  -x1 float
+       	to X (default 1)
+  -y0 float
+       	from Y (default -1.2)
+  -y1 float
+       	to Y (default 1.2)
+```
+
+Some interesting examples
+
+```
+mandelbrot -x0=-1 -x1=-0.65 -y0=0.1 -y1=0.3 -step=0.0002 -i=50
+mandelbrot -x0=-0.745 -x1=-0.7 -y0=0.275 -y1=0.3 -step=0.00003 -i=200
+```
+
+
+### Example Output
+
+img here!
--- mandelbrot.go
+++ mandelbrot.go
@@ -1,69 +1,74 @@
 package main
 
 import (
-  "fmt"
-  "image"
-  "image/color"
-  "image/png"
-  "math"
-  "math/cmplx"
-  "bytes"
-  "github.com/gotbadger/terminalImage"
-  "github.com/gotbadger/palettes"
+	"bytes"
+	"flag"
+	"fmt"
+	"github.com/gotbadger/palettes"
+	"github.com/gotbadger/terminalImage"
+	"image"
+	"image/color"
+	"image/png"
+	"math"
+	"math/cmplx"
 )
 
-const (
-  maxIterations = 30
-  step float64 = 0.003
-)
-//
-func mandelbrot(x float64, y float64) uint8 {
-  a := complex(x,y)
-  var i uint8 = 0
-  for z := a; cmplx.Abs(z) < 2 && i < maxIterations; i++ {
-      z = z*z + a
-  }
-  return i
+func mandelbrot(x float64, y float64, imax int) int {
+	a := complex(x, y)
+	var i int = 0
+	for z := a; cmplx.Abs(z) < 2 && i < imax; i++ {
+		z = z*z + a
+	}
+	return i
 }
 
-func transposeCoordiante(i int, min float64) float64 {
-  return min + (step * float64(i))
+func transposeCoordiante(i int, min float64, step float64) float64 {
+	return min + (step * float64(i))
 }
 
-func getRange(min float64, max float64) int {
-  return int(math.Floor((max-min)/step))
+func getRange(min float64, max float64, step float64) int {
+	return int(math.Floor((max - min) / step))
 }
 
 func main() {
 
-  var yMMin, yMMax float64 = -1.2, 1.2
-  var xMMin, xMMax float64 = -2.0, 1.0
+	// comand line options
+	step := flag.Float64("step", 0.003, "a pixel is drawn for each step between coordinates")
+	maxIterations := flag.Int("i", 30, "max number of iterations / colours")
+	yMMin := flag.Float64("y0", -1.2, "from Y")
+	yMMax := flag.Float64("y1", 1.2, "to Y")
+	xMMin := flag.Float64("x0", -2.0, "from X")
+	xMMax := flag.Float64("x1", 1.0, "to X")
+	flag.Parse()
 
-  // transparent := color.RGBA{0,0,0,0}
-  black := color.RGBA{0,0,0,255}
-  colours := palettes.Rainbow(maxIterations)
+	// transparent := color.RGBA{0,0,0,0}
+	black := color.RGBA{0, 0, 0, 255}
+	colours := palettes.Rainbow(*maxIterations)
 
-  h := getRange(yMMin, yMMax)
-  w := getRange(xMMin, xMMax)
+	h := getRange(*yMMin, *yMMax, *step)
+	w := getRange(*xMMin, *xMMax, *step)
+	fmt.Println("maxIterations:", *maxIterations)
+	fmt.Printf("Generating mandelbrot W:%dpx H:%dpx\n", w, h)
 
-  fmt.Printf("Generating mandelbrot W:%dpx H:%dpx\n", w, h)
+	img := image.NewRGBA(image.Rect(0, 0, w, h))
 
-  img := image.NewRGBA(image.Rect(0, 0, w, h))
+	for x := 0; x < w; x++ {
+		for y := 0; y < h; y++ {
 
-  for x:=0;  x < w; x++ {
-    for y:=0; y < h; y++ {
+			depth := mandelbrot(
+				transposeCoordiante(x, *xMMin, *step),
+				transposeCoordiante(y, *yMMin, *step),
+				*maxIterations)
+			shade := black
+			if depth < *maxIterations {
+				shade = colours[depth]
+			}
 
-      depth := mandelbrot(transposeCoordiante(x, xMMin),transposeCoordiante(y, yMMin))
-      shade := black
-      if(depth < maxIterations){
-        shade = colours[int(depth)]
-      }
+			img.Set(x, y, shade)
+		}
+	}
 
-      img.Set(x, y, shade)
-    }
-  }
-
-  buf := new(bytes.Buffer)
-  png.Encode(buf, img)
-  terminalImage.Print(buf.Bytes())
+	buf := new(bytes.Buffer)
+	png.Encode(buf, img)
+	terminalImage.Print(buf.Bytes())
 }
